ffbf9da0f2167f43b51116b7107fbd9794e15001
Bob Olde Hampsink <bob@robuust.digital>
2023-01-04 10:09:18 +0100

    Remove redirects

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,6 @@
 
 namespace robuust\clientonline;
 
-use Craft;
 use robuust\clientonline\models\Settings;
 use robuust\clientonline\services\ClientOnline;
 
@@ -22,17 +21,6 @@ class Plugin extends \craft\base\Plugin
         $this->setComponents([
             'clientonline' => ClientOnline::class,
         ]);
-
-        // Redirects
-        $request = Craft::$app->getRequest();
-        if (!$request->getIsConsoleRequest() && $request->getQueryParam('co2_news_task', false)) {
-            $articleId = $request->getQueryParam('article_id');
-            $article = $this->clientonline->getEntry($articleId);
-            if ($article) {
-                Craft::$app->getResponse()->redirect($article->url, 301);
-                Craft::$app->end();
-            }
-        }
     }
 
     /**
